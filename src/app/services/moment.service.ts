fda901aa38a16e14c17d7e42f9274ef060b8752b
Revivedaniel <mlg.danielstark@gmail.com>
2024-01-12 19:09:02 -0800

    Added ability to move moments up and down on the timeline and data is now stored in idb

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,25 +1,105 @@
 import { Injectable } from '@angular/core';
 import { Moment } from '../models/moment.model';
+import { Subject } from 'rxjs';
+import { openDB } from 'idb';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MomentService {
-  private moments: Moment[] = [
-    { id: 1, title: 'First moment', text: "asdfasfdasdfasdffdvcvfa", image: "assets/timeline-icons/Red_Blue_Blue.webp" },
-    { id: 2, title: 'Second moment', text: "asdfasfdasdfasdffdvcvfa", image: "assets/timeline-icons/Red_Blue_Blue.webp" },
-    { id: 3, title: 'Third moment', text: "asdfasfdasdfasdffdvcvfa", image: "assets/timeline-icons/Red_Blue_Blue.webp" },
-    { id: 4, title: 'Fourth moment', text: "asdfasfdasdfasdffdvcvfa", image: "assets/timeline-icons/Red_Blue_Blue.webp" },
-    // ... more moments
-  ];
+  private dbPromise: any;
+  private moments: Moment[] = [];
+  momentsUpdated: Subject<Moment[]> = new Subject<Moment[]>();
 
-  constructor() {}
+  constructor() {
+    this.dbPromise = openDB('momentsDB', 1, {
+      upgrade(db) {
+        db.createObjectStore('moments', { keyPath: 'id', autoIncrement: true });
+      },
+    });
+    this.initializeMoments().then((moments) => {
+      this.moments = moments;
+      this.handleMomentUpdate();
+    });
+  }
+
+  async initializeMoments() {
+    const db = await this.dbPromise;
+    return db.getAll('moments');
+  }
 
   getMoments(): Moment[] {
     return this.moments;
   }
 
-  addMoment(moment: Moment) {
+  async addMoment(moment: Moment, delayUpdate?: boolean) {
+    const db = await this.dbPromise;
+    const tx = db.transaction('moments', 'readwrite');
+    await tx.store.add(moment);
+    await tx.done;
     this.moments.push(moment);
+    if (!delayUpdate) {
+      console.log('handleMomentUpdate from addMoment');
+      this.handleMomentUpdate();
+    }
+  }
+
+  async deleteMoment(momentId: number) {
+    const db = await this.dbPromise;
+    const tx = db.transaction('moments', 'readwrite');
+    await tx.store.delete(momentId);
+    await tx.done;
+    this.moments = this.moments.filter((moment) => moment.id !== momentId);
+    this.handleMomentUpdate();
+  }
+
+  async updateMoment(moment: Moment) {
+    const db = await this.dbPromise;
+    const tx = db.transaction('moments', 'readwrite');
+    await tx.store.put(moment);
+    await tx.done;
+    const index = this.moments.findIndex((m) => m.id === moment.id);
+    this.moments[index] = moment;
+    this.handleMomentUpdate();
+  }
+
+  moveMomentUp(moment: Moment) {
+    const index = this.moments.findIndex((m) => m.id === moment.id);
+    if (index > 0) {
+      const previousMoment = this.moments[index - 1];
+      this.moments[index - 1] = moment;
+      this.moments[index] = previousMoment;
+    }
+    this.resetDb(this.moments);
+  }
+
+  moveMomentDown(moment: Moment) {
+    const index = this.moments.findIndex((m) => m.id === moment.id);
+    if (index < this.moments.length - 1) {
+      const nextMoment = this.moments[index + 1];
+      this.moments[index + 1] = moment;
+      this.moments[index] = nextMoment;
+    }
+    this.resetDb(this.moments)
+  }
+
+  resetDb(moments?: Moment[]) {
+    this.dbPromise.then(async (db: any) => {
+      const tx = db.transaction('moments', 'readwrite');
+      await tx.store.clear();
+      await tx.done;
+      this.moments = [];
+      if (moments) {
+        moments.forEach((moment) => {
+          this.addMoment(moment, true);
+        });
+        console.log('handleMomentUpdate from resetDb');
+        this.handleMomentUpdate();
+      }
+    });
+  }
+
+  handleMomentUpdate() {
+    this.momentsUpdated.next(this.moments);
   }
 }
